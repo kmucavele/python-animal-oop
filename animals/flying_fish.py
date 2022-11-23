4afa1b61fb4c6c6afcd9ffc952c6553fa6f5b827
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-11-23 21:16:19 +0100

    Mod: added swimming animals

---
+++ animals/flying_fish.py
@@ -0,0 +1,43 @@
+from datetime import date
+
+from animal_definitions.diet import Diet
+from animal_definitions.gender import Gender
+from animal_types.flying_animal import FlyingAnimals
+from animal_types.swimming_animal import SwimmingAnimals
+
+
+class FlyingFish(FlyingAnimals, SwimmingAnimals):
+    def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, is_alive: bool,
+                 lays_eggs: bool):
+        super().__init__(name, gender, species, diet, birthday, is_alive, True)
+
+    def fly(self) -> None:
+        self.__jump()
+        print(f"And glides")
+
+    def move(self) -> None:
+        pass
+
+    def play(self) -> None:
+        print(f"{self._action_str_prefix()} practicing gliding")
+
+    def defend(self) -> None:
+        print(f"{self.name} ({self.species}) was eaten")
+        self.is_alive = False
+
+    def make_sound(self) -> None:
+        print(f"{self.name}: 'Blub, Blub!'")
+
+    def clean_wings(self) -> None:
+        pass
+
+    def breath(self) -> None:
+        self.__jump()
+        print(f"And takes a deep breath")
+
+    def __jump(self):
+        print(f"{self._action_str_prefix()} jumping out of the water")
+
+    def spread_wings_underwater(self) -> None:
+        print(f"{self._action_str_prefix()} spreading its wings underwater")
+
--- animals/octopus.py
+++ animals/octopus.py
@@ -11,25 +11,28 @@ class Octopus(SwimmingAnimals):
         self.__arms: int = 8
 
     @property
-    def arms(self):
+    def arms(self) -> int:
         return self.__arms
 
     def play(self) -> None:
         print(f"{self._action_str_prefix()} playing catch with a toy")
 
     def defend(self) -> None:
-        print(f"{self._action_str_prefix()} releasing ink")
+        print(f"{self._action_str_prefix()} releasing ink and looking for a place to hide")
 
     def make_sound(self) -> None:
         print("...")
 
+    def move(self) -> None:
+        print(f"{self._action_str_prefix()} walking with its {self.__arms} arms")
+
     def camouflage(self) -> None:
         print(f"{self._action_str_prefix()} disguising by camouflage")
 
-    def perform_arm_autonomy(self):
+    def perform_arm_autonomy(self) -> None:
         self.__arms -= 1
 
-    def grow_arm_back(self):
+    def grow_arm_back(self) -> None:
         assert self.__arms < 8, f"{self.name} already has its 8 arms"
         self.__arms += 1
 
