09a3f85e242c53569c469fbc50c12396a730d1a1
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-11-23 20:32:14 +0100

    Mod: removed abstract methods (are already defined in animal class)

--- animal_types/flying_animal.py
+++ animal_types/flying_animal.py
@@ -6,23 +6,19 @@ from animal_definitions.diet import Diet
 from animal_definitions.gender import Gender
 
 
-class FlyingAnimals(Animal, ABC):
+class FlyingAnimals(Animal):
     def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, is_alive: bool,
                  lays_eggs: bool):
         super().__init__(name, gender, species, diet, birthday, is_alive)
         self.__lays_eggs = lays_eggs
 
     def fly(self) -> None:
-        print(self._action_str_prefix() + " flying")
+        print(f"{self._action_str_prefix()} flying")
 
     @abstractmethod
-    def play(self) -> str:
+    def clean_wings(self) -> None:
         pass
 
-    @abstractmethod
-    def defend(self):
-        pass
 
-    @abstractmethod
-    def make_sound(self) -> str:
-        pass
+if __name__ == '__main__':
+    fly_an = FlyingAnimals(None, None, None, None, None, None, None)
