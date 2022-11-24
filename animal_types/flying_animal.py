9c2354070a9cf3a89952f6def30b2eb7c1904f99
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-11-24 13:47:11 +0100

    Mod: made the lays_eggs property accessible

--- animal_types/flying_animal.py
+++ animal_types/flying_animal.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from datetime import date
 
 from animal_definitions.animal import Animal
@@ -7,10 +7,13 @@ from animal_definitions.gender import Gender
 
 
 class FlyingAnimals(Animal):
-    def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, is_alive: bool,
-                 lays_eggs: bool):
-        super().__init__(name, gender, species, diet, birthday, is_alive)
+    def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, lays_eggs: bool):
+        super().__init__(name, gender, species, diet, birthday)
         self.__lays_eggs = lays_eggs
+
+    @property
+    def lays_eggs(self):
+        return self.__lays_eggs
 
     def fly(self) -> None:
         print(f"{self._action_str_prefix()} flying")
@@ -18,7 +21,3 @@ class FlyingAnimals(Animal):
     @abstractmethod
     def clean_wings(self) -> None:
         pass
-
-
-if __name__ == '__main__':
-    fly_an = FlyingAnimals(None, None, None, None, None, None, None)
