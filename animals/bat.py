fd2345fe969ddfd3aa1fcdbd5b8d5b64ee444757
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-11-24 13:46:29 +0100

    Mod: added ability property and class representation string

--- animals/bat.py
+++ animals/bat.py
@@ -6,9 +6,10 @@ from animal_types.flying_animal import FlyingAnimals
 
 
 class Bat(FlyingAnimals):
-    def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, is_alive: bool,
-                 lays_eggs: bool):
-        super().__init__(name, gender, species, diet, birthday, is_alive, False)
+    def __init__(self, name: str, gender: Gender, species: str, diet: Diet, birthday: date, lays_eggs: bool,
+                 ability=None):
+        super().__init__(name, gender, species, diet, birthday, False)
+        self.__ability = ability
 
     def play(self) -> None:
         print(f"{self._action_str_prefix()} practicing hunting")
@@ -26,8 +27,11 @@ class Bat(FlyingAnimals):
         print(f"{self._action_str_prefix()} cleaning its wings")
 
     def turn_into_a_vampire(self) -> None:
-        self.__species = "🧛🏻"
+        self.__ability = "🧛🏻"
         print(f"Bat ({self.name}) turned into a vampire: {self.species}")
 
     def give_birth(self) -> None:
-        print(f"{self._action_str_prefix()} giving birth")
+        print(f"{self._action_str_prefix()} giving a life birth")
+
+    def __repr__(self) -> str:
+        return f"Animal({self.name}, {self.gender}, {self.species}, {self.birthday}, {self.is_alive}, {self.__ability})"
